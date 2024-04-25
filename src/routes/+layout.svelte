b23450ce7331138e4f1a4691c24b7f153fd61874
Jack Malcom <j@ckmal.com>
2024-04-25 16:53:48 -0500

    add speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,19 @@
 
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 <svelte:head>
 	<title>Jack Malcom</title>
 	<meta name="description" content="Personal website for Jack Malcom" />
-	<meta name="keywords" content="portfolio resume software engineer react angular node.js java spring boot rust" />
+	<meta
+		name="keywords"
+		content="portfolio resume software engineer react angular node.js java spring boot rust"
+	/>
 </svelte:head>
 
 <slot />
